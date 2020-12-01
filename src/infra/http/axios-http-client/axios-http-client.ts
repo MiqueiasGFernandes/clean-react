354d1d2fd55c1c8851d9d2eda868661fcab07937
Miqueias Fernandes <miqueiasgomesfernandes@gmail.com>
2020-12-01 20:18:04 -0300

    feat:  ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,6 +4,6 @@ import axios from 'axios';
 export class AxiosHttpClient {
   // eslint-disable-next-line
   async post(params: HttpPostParams<unknown>): Promise<void>{
-    await axios(params.url);
+    await axios.post(params.url);
   }
 }
