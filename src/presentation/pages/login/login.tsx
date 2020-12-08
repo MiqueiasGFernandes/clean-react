0d65452d18c9435b297c1dafd10a3a6ca33e776c
Miquéias <miqueiasgomesfernandes@gmail.com>
2020-12-07 21:59:07 -0300

    chore: moving Login.tsx elements to components

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import Styles from './login-styles.scss';
-import Spinner from '../../components/spinner/spinner';
-import Header from '../../components/login-header/login-header';
-import Footer from '../../components/footer/footer';
-const Login: React.FC = () => (
-  <div className={Styles.login}>
-    <Header />
-    <form className={Styles.form} action="">
-      <h2>Login</h2>
-      <div className={Styles.inputWrap}>
-        <input type="email" name="email" placeholder="Digite seu e-mail" />
-        <span className={Styles.status}>🔴</span>
-      </div>
-      <div className={Styles.inputWrap}>
-        <input type="password" name="password" placeholder="Digite sua senha" />
-        <span className={Styles.status}>🔴</span>
-      </div>
-      <button className={Styles.submit} type="submit">Entrar</button>
-      <span className={Styles.link}>Criar conta</span>
-      <div className={Styles.errorWrap}>
-        <Spinner className={Styles.spinner} />
-        <span className={Styles.error}>Erro</span>
-      </div>
-    </form>
-    <Footer/>
-  </div>
-);
+import React, { useState } from "react";
+import Styles from "./login-styles.scss";
+import FormStatus from "../../components/form-status/form-status";
+import Header from "../../components/login-header/login-header";
+import Footer from "../../components/footer/footer";
+const Login: React.FC = () => {
+  const [isLoading] = useState(false); 
+  
+
+  return (
+    <div className={Styles.login}>
+      <Header />
+      <form className={Styles.form} action="">
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" placeholder="Digite seu e-mail" />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <button className={Styles.submit} type="submit">
+          Entrar
+        </button>
+        <span className={Styles.link}>Criar conta</span>
+        <FormStatus />
+      </form>
+      <Footer />
+    </div>
+  );
+};
 
 export default Login;
