a728948906f5430320dc1d41b3df2838b5242c01
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-01-01 13:20:53 -0300

    test: ensure ValidationBuilder returns a list of  validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,3 +1,4 @@
+import faker from 'faker';
 import { EmailValidation } from '..';
 import MinLengthValidation from '../min-length/min-length-validation';
 import RequiredFieldValidation from '../required-field/required-field-validation';
@@ -15,5 +16,19 @@ describe('ValidationBuilder', () => {
   test('Should return MinLengthValidation', () => {
     const validations = ValidationBuilder.field('any_field').min(5).build();
     expect(validations).toEqual([new MinLengthValidation('any_field', 5)]);
+  });
+  test('Should return a list of validations', () => {
+    const field = faker.database.column();
+    const length = faker.random.number();
+    const validations = ValidationBuilder.field(field)
+      .required()
+      .email()
+      .min(length)
+      .build();
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new EmailValidation(field),
+      new MinLengthValidation(field, length),
+    ]);
   });
 });
