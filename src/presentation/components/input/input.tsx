37603a7ae9cef116e247dc9624d23a4eb527edf9
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-01-02 13:09:10 -0300

    Revert "test: ensure Input Component loads with readOnly"
    This reverts commit fbe26df2324555fb9705c48f56e6052493b7e93c.

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -26,7 +26,6 @@ const Input: React.FC<Props> = (props: Props) => {
   return (
     <div className={Styles.inputWrap}>
       <input
-        readOnly
         data-testid={name}
         onChange={handleChange}
         type={type}
