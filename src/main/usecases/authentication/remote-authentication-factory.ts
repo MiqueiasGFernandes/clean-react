fdd550d77e4fbac28183d0f9fa50d30d2c73e758
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-01-02 13:37:12 -0300

    refactor: change API URL factory to receive path by param

--- src/main/usecases/authentication/remote-authentication-factory.ts
+++ src/main/usecases/authentication/remote-authentication-factory.ts
@@ -1,8 +1,9 @@
 import RemoteAuthentication from '@/data/usecase/authentication/remote-authentication';
 import { Authentication } from '@/domain/usecases';
+import { makeApiUrl } from '@/main/factories/http/api-factory';
 import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
 
-export const makeRemoteAuthentication = (): Authentication => {
-  const url = 'http://fordevs.herokuapp.com/api/login';
-  return new RemoteAuthentication(url, makeAxiosHttpClient());
-};
+export const makeRemoteAuthentication = (): Authentication => new RemoteAuthentication(
+  makeApiUrl('/login'),
+  makeAxiosHttpClient(),
+);
