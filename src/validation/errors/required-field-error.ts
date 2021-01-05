84740e9f26c9923ed5f71099576a543549dde8e1
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-01-04 21:16:34 -0300

    refactor: remove useless FieldValidation Interface from RequiredFieldError

--- src/validation/errors/required-field-error.ts
+++ src/validation/errors/required-field-error.ts
@@ -1,17 +1,8 @@
 import FieldValidation from '@/validation/protocols/field-validation';
 
-export default class RequiredFieldError extends Error implements FieldValidation {
-  private error: Error;
-
+export default class RequiredFieldError extends Error {
   constructor() {
     super('Campo obrigatório');
     this.name = 'RequiredFieldError';
-  }
-
-  field: string;
-
-  validate(): Error {
-    this.error = new Error('Method not implemented.');
-    return this.error;
   }
 }
