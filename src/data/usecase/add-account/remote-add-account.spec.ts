8641f1b77afa82564d70bf810f06f43e68aa35a2
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-01-15 19:19:24 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if Un HttpPostClient returns 400, 404 or 500

--- src/data/usecase/add-account/remote-add-account.spec.ts
+++ src/data/usecase/add-account/remote-add-account.spec.ts
@@ -50,4 +50,21 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAddAccountParams());
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+    const promise = sut.add(mockAddAccountParams());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+  test('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+    const promise = sut.add(mockAddAccountParams());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
