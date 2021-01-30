77a7bd945de6e70517f5dee131abba024e93ab63
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-01-30 11:06:11 -0300

    feat: ensure signUp starts with correct initial state

--- src/presentation/pages/signUp/signUp.tsx
+++ src/presentation/pages/signUp/signUp.tsx
@@ -1,42 +1,61 @@
 import Context from '@/presentation/context/form/form-context';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Footer from '../../components/footer/footer';
 import FormStatus from '../../components/form-status/form-status';
 import Input from '../../components/input/input';
 import Header from '../../components/login-header/login-header';
 import Styles from './signup-styles.scss';
 
-const SignUp: React.FC = () => (
-  <div className={Styles.signUp}>
-    <Header />
-    <Context.Provider value={{ state: {} }}>
-      <form className={Styles.form}>
-        <h2>Cirar Conta</h2>
-        <Input type="text" name="name" placeholder="Digite seu nome" />
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Digite sua senha"
-        />
-        <Input
-          type="password"
-          name="passwordConfirmation"
-          placeholder="Confirme sua senha"
-        />
-        <button
-          className={Styles.submit}
-          type="submit"
-        >
-          Entrar
-        </button>
-        <Link to="/login" className={Styles.link}>Voltar para Login</Link>
-        <FormStatus />
-      </form>
-    </Context.Provider>
-    <Footer />
-  </div>
-);
+const SignUp: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: '',
+  });
+  return (
+    <div className={Styles.signUp}>
+      <Header />
+      <Context.Provider value={{ state }}>
+        <form className={Styles.form}>
+          <h2>Cirar Conta</h2>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+          />
+          <Input
+            type="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <Input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Confirme sua senha"
+          />
+          <button
+            className={Styles.submit}
+            type="submit"
+            data-testid="submit"
+            disabled
+          >
+            Entrar
+          </button>
+          <span className={Styles.link}>Voltar para Login</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  )
+};
 
 export default SignUp;
