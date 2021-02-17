a511e44a92255abc1e9f1d39f0ce119601ac24d3
Miquéias <miqueiasgomesfernandes@gmail.com>
2021-02-17 20:07:26 -0300

    feat: ensure SignUp calls AddAccount with correct values

--- src/presentation/test/index.ts
+++ src/presentation/test/index.ts
@@ -1,4 +1,5 @@
 export { default as AuthenticationSpy } from './mock-authentication';
 export { default as ValidationStub } from './mock-validation';
 export { default as MockSaveAccessToken } from './mock-save-access-token';
+export { default as AddAccountSpy } from './mock-add-account';
 export * as Helper from './form-helper'
